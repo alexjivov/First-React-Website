5c5837f9103e4b4d15d03ce100e20ac3837c1046
Alex Jivov <alexjivov@gwu.edu>
2018-08-23 08:53:04 -0400

    added gulp dependencies

---
+++ first-react-website/gulpfile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Dependencies
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+
+
+// SCSS/CSS
+
