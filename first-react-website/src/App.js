04b65729e3d28a66b90c670d0790e39df4d94365
Alex Jivov <alexjivov@gwu.edu>
2018-08-25 12:55:57 -0400

    standard header component built and imported into App - no pictures/links

--- first-react-website/src/App.js
+++ first-react-website/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+
+
+// import minified CSS
 import './Assets/css/default.min.css';
 
+
+// import header component
+import Header from './Components/headerComponent/header';
+
+
+
+// components 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       This is my first React Website
+       <Header />
        </div>
     );
   }
---
+++ first-react-website/src/Components/headerComponent/header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+// Removed CSS because it is already included in App.js
+
+// components
+// have to rename component in here and at export - must start with a capital
+class Header extends Component {
+  render() {
+    return (
+     <header>
+        <div className="logo">
+        LOGO
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Products</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+
+            </ul>
+        </nav>
+
+    </header>
+    );
+  }
+}
+
+export default Header;
